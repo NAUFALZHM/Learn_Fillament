59c9eb10a7f94841375c149d2c17dfab7e052b31
NAUFALZULHM <naufalmaulanasans@gmail.com>
2025-07-23 13:26:31 +0700

    livewire

--- warung-app/routes/web.php
+++ warung-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
@@ -13,3 +14,8 @@ Route::view('profile', 'profile')
     ->name('profile');
 
 require __DIR__.'/auth.php';
+
+use App\Livewire\Counter;
+ 
+Route::get('/counter', Counter::class);
+
